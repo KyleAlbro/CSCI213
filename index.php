43c63ace3e45f29b1f5cc33c46e2eb0a6a36a069
KyleAlbro <35697583+KyleAlbro@users.noreply.github.com>
2024-01-17 12:28:35 -0700

    Add files via upload
    added index

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paul's Bakery</title>
+</head>
+
+<body>
+    <h1>Paul's Bakery</h1>
+    <h2>Bakery Items</h2>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Item</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Huckleberry Pie</td>
+                <td>$15.99</td>
+            </tr>
+            <tr>
+                <td>Homemade Rolls (dozen)</td>
+                <td>$8.00</td>
+            </tr>
+            <tr>
+                <td>PLACEHOLDER</td>
+                <td>$2.00</td>
+            </tr>
+</body>
+
+</html>
